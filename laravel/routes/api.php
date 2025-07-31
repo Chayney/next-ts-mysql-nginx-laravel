f6bdb011daeea0ce7b85bae66033e61440de4d7f
Chayney <chunchunmaru33333@gmail.com>
2025-08-01 00:50:01 +0900

    APIエンドポイント設定

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TodoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,9 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user', [AuthController::class, 'user']);
+
+    Route::get('/todos', [TodoController::class, 'index']);
+    Route::post('/todos', [TodoController::class, 'store']);
+    Route::put('/todos', [TodoController::class, 'update']);
+    Route::delete('/todos', [TodoController::class, 'destroy']);
 });
